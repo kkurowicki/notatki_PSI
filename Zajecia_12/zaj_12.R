190fc90dc9da6afa60fcf8b96fec0d3836255742
kkurowicki <k.kurowicki@student.uw.edu.pl>
2026-05-14 17:26:37 +0200

    Add files via upload

---
+++ Zajecia_12/zaj_12.R
@@ -0,0 +1,236 @@
+
+#' ---
+#' title: "TF-IDF"
+#' author: " "
+#' date:   " "
+#' output:
+#'   html_document:
+#'     df_print: paged
+#'     theme: readable      # Wygląd (bootstrap, cerulean, darkly, journal, lumen, paper, readable, sandstone, simplex, spacelab, united, yeti)
+#'     highlight: kate      # Kolorowanie składni (haddock, kate, espresso, breezedark)
+#'     toc: true            # Spis treści
+#'     toc_depth: 3
+#'     toc_float:
+#'       collapsed: false
+#'       smooth_scroll: true
+#'     code_folding: show    
+#'     number_sections: false # Numeruje nagłówki (lepsza nawigacja)
+#' ---
+
+
+knitr::opts_chunk$set(
+  message = FALSE,
+  warning = FALSE
+)
+
+
+
+
+
+#' # Wymagane pakiety
+# Wymagane pakiety ----
+library(tm)
+library(tidyverse)
+library(tidytext)
+library(wordcloud)
+library(ggplot2)
+library(ggthemes)
+
+
+
+#' # Dane tekstowe
+# Dane tekstowe ----
+
+# Ustaw Working Directory!
+# Załaduj dokumenty z folderu
+# docs <- DirSource("textfolder2")
+# W razie potrzeby dostosuj ścieżkę
+# np.: docs <- DirSource("C:/User/Documents/textfolder2")
+
+
+# Utwórz korpus dokumentów tekstowych
+
+# Gdy tekst znajduje się w jednym pliku csv:
+data <- read.csv("LOT_reviews.csv", stringsAsFactors = FALSE, encoding = "UTF-8")
+corpus <- VCorpus(VectorSource(data$Review_Text))
+
+
+# Korpus
+# inspect(corpus)
+
+
+# Korpus - zawartość przykładowego elementu
+corpus[[1]]
+corpus[[1]][[1]]
+corpus[[1]][2]
+
+
+
+#' # 1. Przetwarzanie i oczyszczanie tekstu
+# 1. Przetwarzanie i oczyszczanie tekstu ----
+# (Text Preprocessing and Text Cleaning)
+
+
+# Normalizacja i usunięcie zbędnych znaków ----
+
+# Zapewnienie kodowania w całym korpusie
+corpus <- tm_map(corpus, content_transformer(function(x) iconv(x, to = "UTF-8", sub = "byte")))
+
+
+# Funkcja do zamiany znaków na spację
+toSpace <- content_transformer(function (x, pattern) gsub(pattern, " ", x))
+
+
+# Usuń zbędne znaki lub pozostałości url, html itp.
+
+# symbol @
+corpus <- tm_map(corpus, toSpace, "@")
+
+# symbol @ ze słowem (zazw. nazwa użytkownika)
+corpus <- tm_map(corpus, toSpace, "@\\w+")
+
+# linia pionowa
+corpus <- tm_map(corpus, toSpace, "\\|")
+
+# tabulatory
+corpus <- tm_map(corpus, toSpace, "[ \t]{2,}")
+
+# CAŁY adres URL:
+corpus <- tm_map(corpus, toSpace, "(s?)(f|ht)tp(s?)://\\S+\\b")
+
+# http i https
+corpus <- tm_map(corpus, toSpace, "http\\w*")
+
+# tylko ukośnik odwrotny (np. po http)
+corpus <- tm_map(corpus, toSpace, "/")
+
+# pozostałość po re-tweecie
+corpus <- tm_map(corpus, toSpace, "(RT|via)((?:\\b\\W*@\\w+)+)")
+
+# inne pozostałości
+corpus <- tm_map(corpus, toSpace, "www")
+corpus <- tm_map(corpus, toSpace, "~")
+corpus <- tm_map(corpus, toSpace, "â€“")
+
+
+# Sprawdzenie
+corpus[[1]][[1]]
+
+corpus <- tm_map(corpus, content_transformer(tolower))
+corpus <- tm_map(corpus, removeNumbers)
+corpus <- tm_map(corpus, removeWords, stopwords("english"))
+corpus <- tm_map(corpus, removePunctuation)
+corpus <- tm_map(corpus, stripWhitespace)
+
+
+# Sprawdzenie
+corpus[[1]][[1]]
+
+# usunięcie ewt. zbędnych nazw własnych
+corpus <- tm_map(corpus, removeWords, c("flight", "lot"))
+
+corpus <- tm_map(corpus, stripWhitespace)
+
+# Sprawdzenie
+corpus[[1]][[1]]
+
+
+
+# Decyzja dotycząca korpusu ----
+# do dalszej analizy użyj:
+#
+# - corpus (oryginalny, bez stemmingu)
+#
+
+
+
+
+#' # Tokenizacja
+# Tokenizacja ----
+
+
+
+#' # A. Macierz częstości TDM ----
+# A. Macierz częstości TDM ----
+
+tdm <- TermDocumentMatrix(corpus)
+tdm_m <- as.matrix(tdm)
+
+
+
+#' # 2. Zliczanie częstości słów
+# 2. Zliczanie częstości słów ----
+# (Word Frequency Count)
+
+
+# Zlicz same częstości słów w macierzach
+v <- sort(rowSums(tdm_m), decreasing = TRUE)
+tdm_df <- data.frame(word = names(v), freq = v)
+head(tdm_df, 10)
+
+
+
+#' # 3. Eksploracyjna analiza danych
+# 3. Eksploracyjna analiza danych ----
+# (Exploratory Data Analysis, EDA)
+
+
+# Chmura słów (globalna)
+wordcloud(words = tdm_df$word, freq = tdm_df$freq, min.freq = 7, 
+          colors = brewer.pal(8, "Dark2"))
+
+
+# Wyświetl top 10
+print(head(tdm_df, 10))
+
+
+
+#' # B. Macierz częstości TDM z TF-IDF ----
+# B. Macierz częstości TDM z TF-IDF ----
+
+tdm_tfidf <- TermDocumentMatrix(corpus,
+                                control = list(weighting = function(x) weightTfIdf(x, normalize = FALSE)))
+
+tdm_tfidf
+# inspect(tdm_tfidf)
+
+
+tdm_tfidf_m <- as.matrix(tdm_tfidf)
+
+
+#' # 2. Zliczanie częstości słów
+# 2. Zliczanie częstości słów ----
+# (Word Frequency Count)
+
+
+# Zlicz same częstości słów w macierzach
+v_tfidf <- sort(rowSums(tdm_tfidf_m), decreasing = TRUE)
+tdm_tfidf_df <- data.frame(word = names(v_tfidf), freq = v_tfidf)
+head(tdm_tfidf_df, 10)
+
+
+
+#' # 3. Eksploracyjna analiza danych
+# 3. Eksploracyjna analiza danych ----
+# (Exploratory Data Analysis, EDA)
+
+
+# Chmura słów (globalna)
+wordcloud(words = tdm_tfidf_df$word, freq = tdm_tfidf_df$freq, min.freq = 7, 
+          colors = brewer.pal(8, "Dark2"))
+
+
+# Wyświetl top 10
+print(head(tdm_tfidf_df, 10))
+
+
+
+
+
+
+
+
+
+
+
+
